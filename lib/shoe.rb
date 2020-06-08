06f2e8e79e36be8b66524ea37c1bdeb3121073f6
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 07:50:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Book
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
